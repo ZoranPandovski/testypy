d8501b64f99b61d2112069682b3cd86715ffe8e2
Zoran Pandovski <zoran.pandovski@keitaro.com>
2019-11-29 16:51:49 +0100

    Simple that works

--- setup.py
+++ setup.py
@@ -24,19 +24,8 @@ sys_platform = sys.platform
 with open('requirements.txt') as req_file:
     requirements = [req.strip() for req in req_file.read().splitlines()]
 
-# Linux specific requirements
-if sys_platform == 'linux' or sys_platform.startswith('linux'):
-    pass
-    #requirements = remove_requirements(requirements,'torch',replace='torch >= 1.1.0')
-
-
-# OSX specific requirements
-elif sys_platform == 'darwin':
-    pass
-    #requirements = remove_requirements(requirements,'torch',replace='torch >= 1.1.0.post2')
-
 # Windows specific requirements
-elif sys_platform in ['win32','cygwin','windows']:
+if sys_platform in ['win32','cygwin','windows']:
     requirements = remove_requirements(requirements,'torch')
     requirements = remove_requirements(requirements,'torchvision')
     try:
@@ -47,8 +36,6 @@ elif sys_platform in ['win32','cygwin','windows']:
     except:
         print('Failed to install pytroch, please install pytroch and torchvision manually be following the simple instructions over at: https://pytorch.org/get-started/locally/')
 
-
-    #requirements.append('cwrap')
 
 setuptools.setup(name='testypy',
       version='1.1.1',
